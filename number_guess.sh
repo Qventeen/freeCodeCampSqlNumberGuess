8631f62da16962e23429c18dc07d70280372534c
SerhiiTuhaienko <serhiituhaienko@gmail.com>
2023-08-26 07:53:22 -0400

    refactor: wrap ligin login to separate function

--- number_guess.sh
+++ number_guess.sh
@@ -3,7 +3,11 @@
 #introduce db connect variable
 PSQL="psql -X -U freecodecamp -d number_guess -t --no-align -c"
 
-#take username from input
-echo -e "\nEnter your username:"
+LOGIN() {
+  #take username from input
+  echo -e "\nEnter your username:"
+  read USER_NAME
 
-read USER_NAME
+}
+
+LOGIN

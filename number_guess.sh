b3ab0a9485898f6a02a7b002b6ab8c21ca426d57
SerhiiTuhaienko <serhiituhaienko@gmail.com>
2023-08-26 08:20:56 -0400

    feat: insert new user if such was not find

--- number_guess.sh
+++ number_guess.sh
@@ -11,6 +11,13 @@ LOGIN() {
 
   #select user info
   USER_INFO=$($PSQL "select user_id, name, count(game_id) as games_played, max(number_of_steps) as best_game from users left join games using(user_id) where name = '$USER_NAME' group by user_id")
+   
+  #make step depend on result
+  if [[ -z $USER_INFO ]]
+  then
+    INSERT_NEW_USER_RESULT=$($PSQL "insert into users(name) values('$USER_NAME')")
+    echo -e "\nWelcome, $USER_NAME! It looks like this is your first time here."
+  fi
 }
 
 LOGIN

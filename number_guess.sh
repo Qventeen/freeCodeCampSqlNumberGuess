4d5c90c73cd408f1f5aca586b6908d408fbfdf5a
SerhiiTuhaienko <serhiituhaienko@gmail.com>
2023-08-26 08:04:09 -0400

    feat: Select user info by user name

--- number_guess.sh
+++ number_guess.sh
@@ -8,6 +8,8 @@ LOGIN() {
   echo -e "\nEnter your username:"
   read USER_NAME
 
+  #select user info
+  USER_INFO=$($PSQL "select user_id, name, count(game_id) as games_played, max(number_of_steps) as best_game from users left join games using(user_id) where name = '$USER_NAME' group by user_id")
 }
 
 LOGIN

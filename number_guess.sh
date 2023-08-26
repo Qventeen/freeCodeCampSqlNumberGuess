229b985fbc45b49a2050453b061db5f00f63c1ed
SerhiiTuhaienko <serhiituhaienko@gmail.com>
2023-08-26 10:40:46 -0400

    fix: use correct function for count best game

--- number_guess.sh
+++ number_guess.sh
@@ -10,7 +10,7 @@ LOGIN() {
   read USER_NAME
 
   #select user info
-  USER_INFO=$($PSQL "select user_id, name, count(game_id) as games_played, max(number_of_steps) as best_game from users left join games using(user_id) where name = '$USER_NAME' group by user_id")
+  USER_INFO=$($PSQL "select user_id, name, count(game_id) as games_played, min(number_of_steps) as best_game from users left join games using(user_id) where name = '$USER_NAME' group by user_id")
    
   #make step depend on result
   if [[ -z $USER_INFO ]]
@@ -18,9 +18,9 @@ LOGIN() {
     INSERT_NEW_USER_RESULT=$($PSQL "insert into users(name) values('$USER_NAME')")
     echo -e "\nWelcome, $USER_NAME! It looks like this is your first time here."
   else
-    echo "$USER_INFO" | while IFS="|" read USER_ID NAME GAMES_PLAYED BEST_GAME
+    echo $USER_INFO | while IFS="|" read USER_ID NAME GAMES_PLAYED BEST_GAME
     do
-      echo -e "Welcome back, $NAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
+      echo -e "\nWelcome back, $NAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
     done
   fi
 }
